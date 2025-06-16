c993ed9852064212bb09e8a05f7b76ad81873221
marscodeshere <102072700+marscodeshere@users.noreply.github.com>
2025-06-15 23:04:59 -0400

    Style Changes and Added Content
    Change Nav and added to Home.

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,52 @@
-import Container from "react-bootstrap/Container";
+import { Container, Card, CardGroup } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import logo from "./assets/logo3.png"
+
 
 export default function Home() {
     return (
-        <Container fluid>
-            <div>
-                <p>Hello, testing home.</p>
-            </div>
+        <Container fluid className="pageContent">
+            <h1>Marley Omens</h1>
+            <p>IT Professional / Certified AWS & Web Developer / Artist</p>
+            
+            <CardGroup>
+                <Card>
+                    <Card.Img src={logo} alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This content is a little bit longer.
+                        </Card.Text>
+                        <Card.Text>Last updated 3 mins ago</Card.Text>
+                    </Card.ImgOverlay>
+                </Card>
+                <Card>
+                    <Card.Img src={logo} alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This content is a little bit longer.
+                        </Card.Text>
+                        <Card.Text>Last updated 3 mins ago</Card.Text>
+                    </Card.ImgOverlay>
+                </Card>
+                <Card>
+                    <Card.Img src={logo} alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This content is a little bit longer.
+                        </Card.Text>
+                        <Card.Text>Last updated 3 mins ago</Card.Text>
+                    </Card.ImgOverlay>
+                </Card>
+            </CardGroup>
+
+
         </Container>
     );
 }
