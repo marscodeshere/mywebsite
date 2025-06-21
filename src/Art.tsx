d7d1701c3118ee1b476b0d22ad7cde9dd3301fc5
marscodeshere <102072700+marscodeshere@users.noreply.github.com>
2025-06-21 16:13:29 -0400

    Updated Home and Art Pages
    Moved content from Home to Art page.
    Created new css file for Art page and moved styles from App.css to Art.css.

--- src/Art.tsx
+++ src/Art.tsx
@@ -1,12 +1,52 @@
-import Container from "react-bootstrap/Container";
+import {Container, Card, CardGroup} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./assets/logo3.png";
+import "./Art.css";
 
 export default function Home() {
     return (
-        <Container fluid>
-            <div>
-                <p>Hello, testing art.</p>
-            </div>
+        <Container fluid className="pageContent">
+            <CardGroup>
+                <Card>
+                    <Card.Img src={logo} alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This content is a little bit longer.
+                        </Card.Text>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card.ImgOverlay>
+                </Card>
+                <Card>
+                    <Card.Img src={logo} alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This content is a little bit longer.
+                        </Card.Text>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card.ImgOverlay>
+                </Card>
+                <Card>
+                    <Card.Img src={logo} alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This content is a little bit longer.
+                        </Card.Text>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card.ImgOverlay>
+                </Card>
+            </CardGroup>
         </Container>
     );
 }
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,7 @@
-import { Container, Card, CardGroup } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import logo from "./assets/logo3.png"
+
 
 
 export default function Home() {
@@ -10,49 +10,6 @@ export default function Home() {
             <h1>Marley Omens</h1>
             <p>IT Professional / Certified AWS & Web Developer / Artist</p>
             
-            <CardGroup>
-                <Card>
-                    <Card.Img src={logo} alt="Card image" />
-                    <Card.ImgOverlay>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in
-                        to additional content. This content is a little bit longer.
-                        </Card.Text>
-                        <Card.Footer>
-                            <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
-                    </Card.ImgOverlay>
-                </Card>
-                <Card>
-                    <Card.Img src={logo} alt="Card image" />
-                    <Card.ImgOverlay>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in
-                        to additional content. This content is a little bit longer.
-                        </Card.Text>
-                        <Card.Footer>
-                            <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
-                    </Card.ImgOverlay>
-                </Card>
-                <Card>
-                    <Card.Img src={logo} alt="Card image" />
-                    <Card.ImgOverlay>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in
-                        to additional content. This content is a little bit longer.
-                        </Card.Text>
-                        <Card.Footer>
-                            <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
-                    </Card.ImgOverlay>
-                </Card>
-            </CardGroup>
-
-
         </Container>
     );
 }
