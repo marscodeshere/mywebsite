375355f3c8c471795a3ddf9f9bfced07e29cc8b6
marscodeshere <102072700+marscodeshere@users.noreply.github.com>
2025-06-21 18:01:18 -0400

    Updated Nav and Art
    Fixed some issues with nav in App.tsx.
    Added formatting changes to Art.tsx and changed App.css to configure the changes.

--- src/Art.tsx
+++ src/Art.tsx
@@ -6,6 +6,58 @@ import "./Art.css";
 export default function Home() {
     return (
         <Container fluid className="pageContent">
+            <h1>Marley Omens</h1>
+            <p>The Art Studio: Recent Projects</p>
+
+            <Card className="artHeaders">
+                <Card.Body><h3>Halloween 2023</h3></Card.Body>
+            </Card>
+            
+            <CardGroup>
+                <Card>
+                    <Card.Img src={logo} alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This content is a little bit longer.
+                        </Card.Text>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card.ImgOverlay>
+                </Card>
+                <Card>
+                    <Card.Img src={logo} alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This content is a little bit longer.
+                        </Card.Text>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card.ImgOverlay>
+                </Card>
+                <Card>
+                    <Card.Img src={logo} alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This content is a little bit longer.
+                        </Card.Text>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card.ImgOverlay>
+                </Card>
+            </CardGroup>
+
+            <Card className="artHeaders">
+                <Card.Body><h3>Halloween 2024</h3></Card.Body>
+            </Card>
             <CardGroup>
                 <Card>
                     <Card.Img src={logo} alt="Card image" />
