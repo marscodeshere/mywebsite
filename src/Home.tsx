841b3b9bb4cb4d7711f076bf9a982465ed811d8a
marscodeshere <102072700+marscodeshere@users.noreply.github.com>
2025-06-21 17:06:24 -0400

    Fixed Home Page and Styles
    Edited format of Art.css.
    Added carousel to App.tsx and edited the styles for it in App.css.

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,15 +1,44 @@
-import { Container } from "react-bootstrap";
+import { Container, Carousel, Image } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./assets/logo3.png";
 import "./App.css";
 
 
 
 export default function Home() {
+
+
+
     return (
         <Container fluid className="pageContent">
             <h1>Marley Omens</h1>
             <p>IT Professional / Certified AWS & Web Developer / Artist</p>
             
+            <Carousel pause="hover" controls={false} touch={true}>
+                <Carousel.Item interval={3000}>
+                    <Image src={logo} className="carouselImage"/>
+                    <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    <Image src={logo} className="carouselImage"/>
+                    <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    <Image src={logo} className="carouselImage"/>
+                    <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                </Carousel>
         </Container>
     );
 }
