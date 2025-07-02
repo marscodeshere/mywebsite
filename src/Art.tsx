f44e7e72e3f780b0b0a65e49ec2628c423552488
marscodeshere <102072700+marscodeshere@users.noreply.github.com>
2025-07-02 13:16:59 -0400

    Update Art.tsx
    Small fixes on Art.tsx to correct missing photo and text on photos.

--- src/Art.tsx
+++ src/Art.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         {header: "Halloween 2024", photo1: "MPZX3956.JPG", photo2: "IMG_1805[1].JPG", photo3: "IMG_1781[1].JPG", title1: "Disney Portraits", title2: "Nightmare Before Christmas", title3: "Storm from X-Men", year: "2024"},
         {header: "Cosplay 2023", photo1: "IMG_1177.JPG", photo2: "IMG_1141.JPG", photo3: "IMG_1148.JPG", title1: "Holiday Matsuri 2023", title2: "Old vs New", title3: "Printed Cerberus Skull", year: "2023"},
         {header: "Halloween 2023", photo1: "IMG_1060.JPG", photo2: "IMG_1058.JPG", photo3: "IMG_1002.JPG", title1: "Giant Gengar", title2: "The Crew", title3: "Guess that Pokemon", year: "2023"},
-        {header: "Cosplay 2022", photo1: "IMG_0669.JPG", photo2: "IMG_1152.JPG", photo3: "3dmodel4.png", title1: "Holiday Matsuri 2022", title2: "Emblem for 2023", title3: "Crown for 2023", year: "2022"},
+        {header: "Cosplay 2022", photo1: "IMG_0669.PNG", photo2: "IMG_1152.JPG", photo3: "3dmodel4.png", title1: "Holiday Matsuri 2022", title2: "Emblem for 2023", title3: "Crown for 2023", year: "2022"},
         {header: "Halloween 2022", photo1: "IMG_1103.JPG", photo2: "IMG_0493.JPG", photo3: "MWGL8486.JPG", title1: "Demogorgon", title2: "Vecna", title3: "Demogorgon Final", year: "2022"},
         {header: "3D Models", photo1: "3dmodel1.png", photo2: "3dmodel2.png", photo3: "3dmodel3.png", title1: "Cerberus Skull Model", title2: "Shield Model", title3: "Skull Belt Model", year: "2024"},
     ];
@@ -35,7 +35,7 @@ export default function Home() {
                         <Card.ImgOverlay>
                             <Card.Title>{a.title1}</Card.Title>
                             <Card.Footer>
-                            <small className="text-muted">{a.year}</small>
+                            <small className="text-muted">Last updated: {a.year}</small>
                             </Card.Footer>
                         </Card.ImgOverlay>
                     </Card>
@@ -44,7 +44,7 @@ export default function Home() {
                         <Card.ImgOverlay>
                             <Card.Title>{a.title2}</Card.Title>
                             <Card.Footer>
-                            <small className="text-muted">{a.year}</small>
+                            <small className="text-muted">Last updated: {a.year}</small>
                             </Card.Footer>
                         </Card.ImgOverlay>
                     </Card>
@@ -53,7 +53,7 @@ export default function Home() {
                         <Card.ImgOverlay>
                             <Card.Title>{a.title3}</Card.Title>
                             <Card.Footer>
-                            <small className="text-muted">{a.year}</small>
+                            <small className="text-muted">Last updated: {a.year}</small>
                             </Card.Footer>
                         </Card.ImgOverlay>
                     </Card>
